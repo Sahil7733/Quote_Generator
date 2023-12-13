825a44f9e534677176677ce8fd703357147df48e
Abhisek Sahu <143437904+Sahil7733@users.noreply.github.com>
2023-12-13 23:26:11 +0530

    random quote generator

--- app/src/androidTest/java/com/federicocotogno/quotegenerator/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/federicocotogno/quotegenerator/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.federicocotogno.quotegenerator
+package com.abhisek.quotegenerator
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.federicocotogno.quotegenerator", appContext.packageName)
+        assertEquals("com.abhisek.quotegenerator", appContext.packageName)
     }
 }
